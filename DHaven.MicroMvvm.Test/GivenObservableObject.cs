7f68566c7f9090a1f14bf213bc08cf06f349d99f
Berin Loritsch <berin@d-haven.org>
2017-05-15 20:03:32 -0400

    First cut to release MicroMvvm core

---
+++ DHaven.MicroMvvm.Test/GivenObservableObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DHaven.MicroMvvm.Test
+{
+    public class GivenObservableObject
+    {
+        public class TestObservable : ObservableObject
+        {
+            public string Name
+            {
+                get => GetValue<string>(nameof(Name));
+                set => SetValue(nameof(Name), value);
+            }
+
+            public double X
+            {
+                get => GetValue<int>(nameof(X));
+                set => SetValue(nameof(X), value);
+            }
+        }
+
+        [Fact]
+        public void PropertiesShouldBehaveNormally()
+        {
+            var testObject = new TestObservable();
+
+            Assert.Null(testObject.Name);
+
+            testObject.Name = "Test String";
+
+            Assert.Equal("Test String", testObject.Name);
+        }
+
+        [Fact]
+        public void PropertiesShouldRaisePropertyChanged()
+        {
+            var propertiesChanged = new List<string>();
+            var testObject = new TestObservable();
+            testObject.PropertyChanged += (source, args) => propertiesChanged.Add(args.PropertyName);
+
+            testObject.Name = "Another String";
+            testObject.X = Math.PI;
+
+            Assert.Equal(2, propertiesChanged.Count);
+            Assert.Contains(nameof(testObject.Name), propertiesChanged);
+            Assert.Contains(nameof(testObject.X), propertiesChanged);
+        }
+    }
+}
---
+++ DHaven.MicroMvvm.Test/GivenViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+
+namespace DHaven.MicroMvvm.Test
+{
+    public class GivenViewModel
+    {
+        private class TestViewModel<T> : ViewModel<T>
+        {
+            public TestViewModel() : base(default(T)) { }
+
+            public TestViewModel(T model) : base(model) { }
+        }
+
+        [Fact]
+        public void ShouldNotRequireModelInConstructor()
+        {
+            var viewModel = new TestViewModel<GivenObservableObject.TestObservable>();
+
+            Assert.Null(viewModel.Model);
+        }
+
+        [Fact]
+        public void ShouldAllowInitialObjectInConstructor()
+        {
+            const string model = "A silly string, not practical, but good enough";
+            var viewModel = new TestViewModel<string>(model);
+
+            Assert.Equal(model, viewModel.Model);
+        }
+
+        [Fact]
+        public void ShouldNotifyModelChanged()
+        {
+            var modelChanged = false;
+            var viewModel = new TestViewModel<int>(2016);
+            viewModel.PropertyChanged += (source, args) => modelChanged = args.PropertyName == nameof(viewModel.Model);
+
+            viewModel.Model = 288;
+
+            Assert.True(modelChanged);
+        }
+    }
+}
